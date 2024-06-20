96a310f93a1a10290bd02a48d95d6a3ce02d12b2
Pipor Games <pipornet@gmail.com>
2024-06-20 18:05:44 +0200

    Initial commit

---
+++ src/main/java/me/piporgames/smppl2core/SMPPL2Core.java
@@ -0,0 +1,17 @@
+package me.piporgames.smppl2core;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SMPPL2Core extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
